c485ffefb2b340f9eca5238d14c530d4e0beddb8
Jitendra Singh <jscmcharan@gmail.com>
2025-03-20 10:49:30 +0530

    Create audiototext.py
    Main file

---
+++ audiototext.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr  
+from pydub import AudioSegment
+
+mp3_file = "/home/kali/Downloads/dimpal.mp3"
+mp3_file = input("Enter complete file path : ")
+wav_file = "converted_audio.wav"
+
+audio = AudioSegment.from_mp3(mp3_file)
+audio.export(wav_file, format="wav")
+
+recognizer = sr.Recognizer()
+
+audio_file = "/home/kali/converted_audio.wav"  
+
+# Open the file and recognize speech
+with sr.AudioFile(audio_file) as source:
+    print("Processing audio file...")
+    audio = recognizer.record(source)  # Read the entire file
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("Transcribed Text:", text)
+    except sr.UnknownValueError:
+        print("Sorry, could not understand the audio.")
+    except sr.RequestError:
+        print("Error with the speech recognition service.")
+
